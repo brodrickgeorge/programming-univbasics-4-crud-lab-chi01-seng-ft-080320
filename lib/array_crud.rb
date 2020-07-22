f8ffd6787f812086905047fafd1e278fecbcaa5f
Brodrick George <brodrick.george@yahoo.com>
2020-07-22 21:20:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,17 +3,17 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Averie", "Jarod", "Nicole", "Brodrick", "Ryan"]
+  ["Averie", "Jarod", "Nicole", "Brodrick"]
 end
 
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
-  array.push = ("arrays!")
+  array.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
    array = ["I", "am", "really", "learning"]
-   array.unshift = ("wow")
+   array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
@@ -43,5 +43,5 @@ end
 
 def update_element_from_index(array, index_number, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array[4] = "totally "
+  array[4] = "totally"
 end
